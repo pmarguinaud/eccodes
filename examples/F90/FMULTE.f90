01c928d106792e8c61ad47e1475297352905593f
Philippe Marguinaud <pmarguinaud@hotmail.com>
2024-09-20 12:13:46 +0200

    Use SURFNEBUL.TOTALE

--- examples/F90/FMULTE.f90
+++ examples/F90/FMULTE.f90
@@ -7,21 +7,17 @@ INTEGER(KIND=8) IMULTM, IMULTE
 
 CALL codes_grib_new_from_samples(IGRIBH, "regular_ll_sfc_grib2")
 
-! set centre to MeteoFrance and use their local definition
-CALL codes_set(IGRIBH, 'centre', 85)
-CALL codes_set(IGRIBH, 'grib2LocalSectionPresent', 1)
-CALL codes_set(IGRIBH, 'grib2LocalSectionNumber', 1)
 
-CLNOMA1 = 'SURFNEBUL.TOTALE'
-CALL codes_set_string(IGRIBH, 'faFieldName', CLNOMA1)
-!CALL codes_get_string(IGRIBH, 'faFieldName', CLNOMA2)
+CALL codes_SET_LONG (IGRIBH, "centre", 85_8 )
+CALL codes_SET_INT (IGRIBH, "grib2LocalSectionPresent", 1 )
+CALL codes_SET_LONG (IGRIBH, "grib2LocalSectionNumber", 1_8 )
+CALL codes_SET_LONG (IGRIBH, "typeOfGeneratingProcess", 2_8 )
+CALL codes_SET_LONG (IGRIBH, "typeOfProcessedData", 1_8 )
+CALL codes_SET_STRING (IGRIBH, "faFieldName", "SURFNEBUL.TOTALE")
+
 
 CALL codes_get (IGRIBH, 'FMULTE', IMULTE)
 
-PRINT *, " CLNOMA1 = ", CLNOMA1
-!PRINT *, " CLNOMA2 = ", CLNOMA2
 PRINT *, " IMULTE  = ", IMULTE
-
-!IF (CLNOMA1 /= CLNOMA2) STOP 1
 
 END
